5cf5b6e72d31b090964fbf39961ed91cdb198d10
sharkbyte <sgquin@gmail.com>
2022-08-05 18:27:46 -0700

    remove technology card boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,29 +18,6 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
           Create <span className="text-purple-300">T3</span> App
         </h1>
-        <p className="text-2xl text-gray-700">This stack uses:</p>
-        <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <TechnologyCard
-            name="NextJS"
-            description="The React framework for production"
-            documentation="https://nextjs.org/"
-          />
-          <TechnologyCard
-            name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
-          />
-          <TechnologyCard
-            name="TailwindCSS"
-            description="Rapidly build modern websites without ever leaving your HTML"
-            documentation="https://tailwindcss.com/"
-          />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
-        </div>
         <div className="flex items-center justify-center w-full pt-6 text-2xl text-blue-500">
           {hello.data ? <div>{hello.data.secondHello}</div> : <p>Loading..</p>}
         </div>
@@ -56,12 +33,15 @@ const Home: NextPage = () => {
               Sign In
             </button>
           ) : (
-            <button
-              className="rounded-xl p-10 btn-primary"
-              onClick={() => signOut()}
-            >
-              Sign Out
-            </button>
+            <div>
+              <h2>You are signed in</h2>
+              <button
+                className="rounded-xl p-10 btn-primary"
+                onClick={() => signOut()}
+              >
+                Sign Out
+              </button>
+            </div>
           )}
         </div>
       </main>
@@ -72,29 +52,3 @@ const Home: NextPage = () => {
 export default Home;
 
 // Technology component
-interface TechnologyCardProps {
-  name: string;
-  description: string;
-  documentation: string;
-}
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
