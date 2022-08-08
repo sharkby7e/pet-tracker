064af691f6bc2e36a4e71aec1f98e3b5fea149aa
sharkbyte <sgquin@gmail.com>
2022-08-07 20:12:13 -0700

    restyle hello page

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,6 +1,12 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>Hi Mom</h1>;
+  return (
+    <>
+      <div className="mt-16 bg-gray-700 p-10">
+        <h1 className="text-5xl text-center text-secondary">Hi Mom</h1>
+      </div>
+    </>
+  );
 };
 export default Home;
